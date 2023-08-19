19fcffa22bedbdd709c529f141b714a2b1c664c8
zaizhong <zaizhong2754@gmail.com>
2023-08-20 00:53:30 +0800

    feat: 引入uni-ui组件库

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,38 +1,34 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+    <uni-card cover="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png">
+      <text>这是一个带封面和操作栏的卡片示例，此示例展示了封面插槽和操作栏插槽的用法。</text>
+      <template v-slot:actions>
+        <view class="card-actions">
+          <view class="card-actions-item" @click="actionsClick('分享')">
+            <uni-icons type="pyq" size="18" color="#999"></uni-icons>
+            <text class="card-actions-item-text">分享</text>
+          </view>
+          <view class="card-actions-item" @click="actionsClick('点赞')">
+            <uni-icons type="heart" size="18" color="#999"></uni-icons>
+            <text class="card-actions-item-text">点赞</text>
+          </view>
+          <view class="card-actions-item" @click="actionsClick('评论')">
+            <uni-icons type="chatbubble" size="18" color="#999"></uni-icons>
+            <text class="card-actions-item-text">评论</text>
+          </view>
+        </view>
+      </template>
+    </uni-card>
   </view>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 const title = ref('Hello')
+
+const actionsClick = (action: string) => {
+  console.log(action)
+}
 </script>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  width: 200rpx;
-  height: 200rpx;
-  margin: 200rpx auto 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-</style>
+<style></style>
